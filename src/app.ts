acb4af01e13093a69a9e4dead1770f3ce89a669a
Michael Swart <michaelswart@Michaels-MacBook-Pro.local>
2017-01-19 08:03:33 -0600

    More jquery

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,12 @@ import * as MyClass from './MyClass';
 import jquery = require('jquery');
 
 jquery(() => {
-    jquery(document.body).html(MyClass.MyClass.Hello("Mike"));
+    // jquery(document.body).html(MyClass.MyClass.Hello("Mike"));
+    jquery(document).ready(() =>
+    {
+        jquery("#goButton").click(() => {
+            jquery("#resultText").html(MyClass.MyClass.Hello(jquery("#name").val()));
+        });
+    });
 })
 // console.log(MyClass.MyClass.Hello("Mike"));
